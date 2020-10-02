8a61d4728ce26ed99147d5d7fe49eb67dd6a74d4
Christina Ninh <christinaninh07@yahoo.com>
2020-10-01 23:35:10 -0700

    initial commit

---
+++ src/FractionsV1.java
@@ -0,0 +1,12 @@
+/** Christina Ninh
+ * CSS143 Kalmin
+ */
+import java.io.File; //import the File class
+import java.io.FileNotFoundException; //Import this class to handle errors
+import java.util.Scanner; //Import the Scanner class to read text files
+
+public class FractionsV1 {
+    public static void main(String[] args) {
+
+    }
+}
