560f55b1dbd4c8e2726b1071ae63b2c4bfca7de4
Christina Ninh <christinaninh07@yahoo.com>
2020-10-02 00:13:55 -0700

    read file

--- src/FractionsV1.java
+++ src/FractionsV1.java
@@ -1,12 +1,20 @@
 /** Christina Ninh
  * CSS143 Kalmin
  */
-import java.io.File; //import the File class
+import java.lang.String;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException; //Import this class to handle errors
 import java.util.Scanner; //Import the Scanner class to read text files
 
 public class FractionsV1 {
     public static void main(String[] args) {
-
-    }
-}
+        //reads file
+        Scanner inputFile = null;
+        try {
+            inputFile = new Scanner(new FileInputStream("fractions.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Text file could not be read.");
+            e.printStackTrace();
+        }//end of try
+    }//end main
+}//end class
