2c0bcd2830bbfaa140593438a2013edb67b28756
Christina Ninh <christinaninh07@yahoo.com>
2020-10-05 12:11:11 -0700

    added comments

--- src/FractionsV1.java
+++ src/FractionsV1.java
@@ -1,25 +1,25 @@
 /** Christina Ninh
  * CSS143 Kalmin
  */
-import java.lang.String;
-import java.io.FileInputStream;
+import java.lang.String; //import to read strings
+import java.io.FileInputStream; //import this class to take files
 import java.io.FileNotFoundException; //Import this class to handle errors
 import java.util.Scanner; //Import the Scanner class to read text files
 
 public class FractionsV1 {
     public static void main(String[] args) {
-        //reads file
+        //reads file with scanner
         Scanner inputFile = null;
         try {
             inputFile = new Scanner(new FileInputStream("fractions.txt"));
         } catch (FileNotFoundException e) {
             System.out.println("Text file could not be read.");
-            e.printStackTrace();
+            e.printStackTrace(); //automated error response, helps trace where problem is
         }//end of try
 
         //set up variables
         String[] fractions = new String[100]; //String to take in fractions
-        String[] split = new String[2]; //used to split the fractions
+        String[] split; //used to split the fractions
         int[] num = new int[100]; //store numerator
         int[] denom = new int[100]; //store denom
         int count = 0; //number of lines in the file
